7a5c5a875c1cf53f70a96e0329bbd02949d30fdf
Frank Pfattheicher <fpf@ict-baden.de>
2013-09-13 07:53:43 +0200

    Default startup mode set to SERVER.

--- IctBaden.StonehengeSample/Program.cs
+++ IctBaden.StonehengeSample/Program.cs
@@ -12,7 +12,7 @@ namespace IctBaden.StonehengeSample
     {
       App = new AppEngine("Stonehenge Sample", "about");
 
-      var asApp = !Environment.CommandLine.ToUpper().Contains("/SERVER");
+      var asApp = Environment.CommandLine.ToUpper().Contains("/APP");
       App.Run(asApp);
 
       if (!asApp)
