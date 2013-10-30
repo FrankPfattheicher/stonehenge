5c2aa99463302394d4502a748c23be8ed366afa3
Frank Pfattheicher <fpf@ict-baden.de>
2013-10-30 19:49:36 +0100

    CDN support for css files. CDN support tests.

--- IctBaden.Stonehenge/Creators/ContentDeliveryNetworkSupport.cs
+++ IctBaden.Stonehenge/Creators/ContentDeliveryNetworkSupport.cs
@@ -2,9 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Security.Cryptography;
 using System.Text.RegularExpressions;
-using System.Web.Profile;
 
 namespace IctBaden.Stonehenge.Creators
 {
@@ -13,36 +11,67 @@ namespace IctBaden.Stonehenge.Creators
     private const string CdnConfigurationFileName = "CDN.cfg";
     private static Dictionary<string, string> cdnLookup;
 
+    public static Dictionary<string, string> CdnLookup
+    {
+      get
+      {
+        if (cdnLookup != null) 
+          return cdnLookup;
 
-    public static string RersolveHosts(string source)
+        if (File.Exists(CdnConfigurationFileName))
+        {
+          cdnLookup = (from line in File.ReadAllLines(CdnConfigurationFileName)
+            where !line.StartsWith("#")
+            let elements = line.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries)
+            where elements.Length == 2
+            select elements).ToDictionary(e => e[0], e => e[1]);
+        }
+        else
+        {
+          cdnLookup = new Dictionary<string, string>();
+        }
+
+        return cdnLookup;
+      }
+      set
+      {
+        cdnLookup = value;
+      }
+    }
+
+    public static string RersolveHosts(string page)
     {
       if (!File.Exists(CdnConfigurationFileName))
-        return source;
-
-      if (cdnLookup == null)
-      {
-        cdnLookup = (from line in File.ReadAllLines(CdnConfigurationFileName)
-                     where !line.StartsWith("#")
-                     let elements = line.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries)
-                     where elements.Length == 2
-                     select elements).ToDictionary(e => e[0], e => e[1]);
-      }
+        return page;
 
       var lines = new List<string>();
 
-      var reg = new Regex("<script type=\"text/javascript\" src=\"scripts/(.*\\.js)\"></script>", RegexOptions.Compiled);
-      foreach (var line in source.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+      var script = new Regex("<script.*src=\"scripts/(.*\\.js)\".*", RegexOptions.Compiled);
+      var css = new Regex("<link.*href=\"css/(.*\\.css)\".*", RegexOptions.Compiled);
+
+      foreach (var line in page.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
       {
-        var isScriptSource = reg.Match(line);
-        if (!isScriptSource.Success)
+        var isScriptSource = script.Match(line);
+        if (isScriptSource.Success)
         {
-          lines.Add(line);
+          var source = isScriptSource.Groups[1].Value;
+          lines.Add(!CdnLookup.ContainsKey(source)
+            ? line
+            : isScriptSource.Groups[0].ToString().Replace(source, CdnLookup[source]));
           continue;
         }
-        var script = isScriptSource.Groups[1].Value;
-        lines.Add(!cdnLookup.ContainsKey(script)
-          ? line
-          : string.Format("<script type=\"text/javascript\" src=\"{0}\"></script>", cdnLookup[script]));
+
+        var isCssSource = css.Match(line);
+        if (isCssSource.Success)
+        {
+          var source = isCssSource.Groups[1].Value;
+          lines.Add(!CdnLookup.ContainsKey(source)
+            ? line
+            : isCssSource.Groups[0].ToString().Replace(source, CdnLookup[source]));
+          continue;
+        }
+
+        lines.Add(line);
       }
 
       return string.Join(Environment.NewLine, lines);
