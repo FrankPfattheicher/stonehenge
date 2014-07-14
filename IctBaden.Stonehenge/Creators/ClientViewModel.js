d27edf21b119baed2aa4a3a956fe06854c4d07c5
Frank Pfattheicher <fpf@ict-baden.de>
2014-07-14 09:23:08 +0200

    Event polling fixed.

--- IctBaden.Stonehenge/Creators/ClientViewModel.js
+++ IctBaden.Stonehenge/Creators/ClientViewModel.js
@@ -1,6 +1,8 @@
 ﻿//ViewModel:_ViewModelType_
 var self;
 var activation_data;
+var loading_ViewModelName_active = false;
+var polling_ViewModelName_active = false;
 // ReSharper disable InconsistentNaming
 // ReSharper disable UseOfImplicitGlobalInFunctionScope
 // ReSharper disable UnusedLocals
@@ -9,14 +11,26 @@ function poll_ViewModelName_Events(viewmodel, initial) {
     var app = require('durandal/app');
     var router = require('plugins/router');
     var ts = new Date().getTime();
+    polling_ViewModelName_active = true;
     $.getJSON('/events/_ViewModelType_?ts=' + ts + '&stonehenge_id=' + stonehenge_id, function (data) {
-        if (data == null) return;
-        set_ViewModelName_Data(viewmodel, false, data);
-        if (data.stonehenge_eval != null) eval(data.stonehenge_eval);
-        if (data.stonehenge_navigate != null) router.navigate(data.stonehenge_navigate);
-        if (initial || (data.stonehenge_poll != null)) setTimeout(function () { poll_ViewModelName_Events(viewmodel, false); }, 200);
+        polling_ViewModelName_active = false;
+        if (!loading_ViewModelName_active && (data != null)) {
+            set_ViewModelName_Data(viewmodel, false, data);
+            if (data.stonehenge_eval != null) eval(data.stonehenge_eval);
+            if (data.stonehenge_navigate != null) router.navigate(data.stonehenge_navigate);
+            if (data.stonehenge_poll != null) initial = true;
+        }
+        if (initial && !polling_ViewModelName_active) {
+            polling_ViewModelName_active = true;
+            setTimeout(function () { poll_ViewModelName_Events(viewmodel, false); }, 200);
+        }
     }).fail(function () {
         //alert("poll_ViewModelName_Events getJSON failed");
+        polling_ViewModelName_active = false;
+        if (!polling_ViewModelName_active) {
+            polling_ViewModelName_active = true;
+            setTimeout(function () { poll_ViewModelName_Events(self, true); }, 200);
+        }
     });
 }
 function set_ViewModelName_Data(viewmodel, loading, data) {
@@ -27,13 +41,15 @@ function set_ViewModelName_Data(viewmodel, loading, data) {
         set_ViewModelName_Data(viewmodel, loading, data);
     }
     if (loading) {
-        if (viewmodel.InitialLoading()) {
+        if (!polling_ViewModelName_active) {
+            polling_ViewModelName_active = true;
             setTimeout(function () { poll_ViewModelName_Events(self, true); }, 200);
         }
         viewmodel.InitialLoading(false);
         viewmodel.IsLoading(false);
         viewmodel.IsDirty(false);
     }
+    loading_ViewModelName_active = false;
 }
 function post_ViewModelName_Data(viewmodel, sender, method, param) {
     //debugger;
@@ -42,6 +58,7 @@ function post_ViewModelName_Data(viewmodel, sender, method, param) {
     _GetData_();
     var ts = new Date().getTime();
     viewmodel.IsLoading(true);
+    loading_ViewModelName_active = true;
     $.post('/viewmodel/_ViewModelType_/' + method + '?ts=' + ts + '&stonehenge_id=' + stonehenge_id, params, function (data) {
         set_ViewModelName_Data(viewmodel, true, data);
     }).fail(function () {
@@ -97,6 +114,7 @@ define(['durandal/app', 'durandal/system', 'knockout', 'flot'], function (app, s
             var ts = new Date().getTime();
             if (stonehenge_id != null)
             {
+                loading_ViewModelName_active = true;
                 $.getJSON('/viewmodel/_ViewModelType_?ts=' + ts + '&stonehenge_id=' + stonehenge_id, function (data) {
                     set_ViewModelName_Data(self, true, data);
                 }).fail(function () {
